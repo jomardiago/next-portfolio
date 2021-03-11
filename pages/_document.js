66088645d651d49dcc12e59784753df2f0e5218b
Jose-Diago <jose.diago@ibm.com>
2021-03-11 15:03:00 +0800

    Fixing some warnings in the terminal.

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,15 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
     render() {
         return (
-            <html lang="en">
+            <Html lang="en">
                 <Head>
                     <meta name="description" content="A site for my programming portfolio"/>
                     <meta charSet="utf-8" />
                     <meta name="robots" content="noindex, nofollow"/>
                     <meta name="viewport" content="width=device-width"/>
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" />
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossOrigin="anonymous" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
                 </Head>
@@ -24,7 +24,7 @@ class MyDocument extends Document {
                         }
                     `}
                 </style>
-            </html>
+            </Html>
         );
     }
 };
