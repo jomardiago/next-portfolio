a8aacec4d5e3bb1429357b8ad2243c84886d7955
Jose-Diago <jose.diago@ibm.com>
2021-03-11 16:02:50 +0800

    11. Cleaner Urls without Query Parameters

--- pages/blog.js
+++ pages/blog.js
@@ -4,9 +4,9 @@ import Link from 'next/link';
 const Blog = () => (
     <Layout title="Blog">
         <ul>
-            <li><Link href="/post?title=React"><a>React</a></Link></li>
-            <li><Link href="/post?title=Angular"><a>Angular</a></Link></li>
-            <li><Link href="/post?title=NextJS"><a>NextJS</a></Link></li>
+            <li><Link as="/react-post" href="/post?title=React"><a>React</a></Link></li>
+            <li><Link as="/angular-post" href="/post?title=Angular"><a>Angular</a></Link></li>
+            <li><Link as="/nextjs-post" href="/post?title=NextJS"><a>NextJS</a></Link></li>
         </ul>
     </Layout>
 );
--- pages/post.js
+++ pages/post.js
@@ -1,9 +1,10 @@
 import Layout from '../components/Layout';
+import { withRouter } from 'next/router';
 
-const Post = ({ url }) => (
-    <Layout title={url.query.title}>
+const Post = ({ router }) => (
+    <Layout title={router.query.title}>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis tempora eveniet neque sit porro suscipit iusto error aspernatur fugiat nobis!</p>
     </Layout>
 );
 
-export default Post;
+export default withRouter(Post);
