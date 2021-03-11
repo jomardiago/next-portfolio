3392468f05e44a619ca6f59c14233cab06170663
Jose-Diago <jose.diago@ibm.com>
2021-03-11 14:57:13 +0800

    7. Custom Document Page

--- components/Layout.js
+++ components/Layout.js
@@ -15,7 +15,6 @@ const Layout = ({ children, title }) => (
     <div className="root">
         <Head>
             <title>Next Portfolio</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" />
         </Head>
         <header>
             <Link href="/"><a>Home</a></Link>
